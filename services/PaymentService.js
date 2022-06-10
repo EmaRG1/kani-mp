7b2836387c6feffba2202a2d05c883a7428df069
alex.villecco <amvillecco@teco.com.ar>
2022-06-10 18:07:31 -0300

    setup kani app to deploy

---
+++ services/PaymentService.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+
+class PaymentService {
+  async createPayment() {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+      payer_email: "test_user_88956061@testuser.com",
+      items: [
+        {
+          title: "Kani producto",
+          description: "Dummy description",
+          picture_url: "http://www.myapp.com/myimage.jpg",
+          category_id: "category123",
+          quantity: 1,
+          unit_price: 10
+        }
+      ],
+      back_urls: {
+        failure: "/failure",
+        pending: "/pending",
+        success: "/success"
+      }
+    };
+
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+
+    return payment.data;
+  }
+
+  async createSubscription() {
+    const url = "https://api.mercadopago.com/preapproval";
+
+    const body = {
+      reason: "Suscripción a Kani",
+      auto_recurring: {
+        frequency: 1,
+        frequency_type: "months",
+        transaction_amount: 2000,
+        currency_id: "ARS"
+      },
+      back_url: "https://kani-test.netlify.app/subscription-success",
+      payer_email: "test_user_88956061@testuser.com" // TODO: hay que usar el email que recibimos por props (?)
+    };
+
+    const subscription = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+
+    return subscription.data;
+  }
+}
+
+module.exports = PaymentService;
